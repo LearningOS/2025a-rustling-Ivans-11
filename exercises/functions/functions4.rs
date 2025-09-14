366420671f989412e18d042318afc09d2f00542e
Ivans-11 <w-yf22@mails.tsinghua.edu.cn>
2025-09-15 01:18:29 +0800

    update

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -12,6 +12,7 @@
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
+    println!("{}", original_price);
 }
 
 fn sale_price(price: i32) -> i32 {
